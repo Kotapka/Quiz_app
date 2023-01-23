e9cf6010bf35ee38048ea6eb8cb1989ff9cdead3
Bartosz Kmiecik <bkmiecik813@gmail.com>
2023-01-23 13:39:24 +0100

    added getters and setters in class Question

--- src/main/java/com/example/Quiz_app/dataBase/Question.java
+++ src/main/java/com/example/Quiz_app/dataBase/Question.java
@@ -32,4 +32,28 @@ public class Question {
         this.answers.add(wrongAnswer2);
         this.answers.add(wrongAnswer3);
     }
+
+    public Integer getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(Integer questionID) {
+        this.questionID = questionID;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
 }
